0a1d6b316e3b2a04a719a4d165c3acb4f9aefefc
Ivan Ponomarev <iponomarev@mail.ru>
2026-06-16 00:42:21 +0100

    versions upgrade (#5)
    * versions upgrade

--- java-maven/src/test/java/ru/inponomarev/backend/controller/Api.java
+++ java-maven/src/test/java/ru/inponomarev/backend/controller/Api.java
@@ -1,18 +1,13 @@
 package ru.inponomarev.backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import ru.inponomarev.backend.dto.ItemDTO;
 import ru.inponomarev.backend.dto.OrderDTO;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface Api {
